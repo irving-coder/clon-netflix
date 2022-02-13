00b10a437bb4befde1d6be9171d05704f629b853
Irving Martínez <irving.mtqz10@yahoo.com>
2022-02-12 23:56:10 -0600

    Efecto de click en input 'new-user'

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-'use script';
+'use strict';
 
 (function(){
   let inputEmail = document.querySelector('[name="email"]');
@@ -6,5 +6,9 @@
 
   /* Queda pendiente evento cuando se enfoque en el input el 'label' se
   recorre hacia arriba y minimiza su tamañan un poco */
-  // inputEmail.addEventListener('');
+  inputEmail.addEventListener('click', () => {
+    labelEmail.style.margin = '0px 5px 0px 0px';
+    labelEmail.style.margin = '0px 0px 0px 5px';
+    labelEmail.style.fontsize = '10px';
+  });
 })();
