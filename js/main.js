67e03090531c54fc5696b337ec08fef347986ae2
Irving Martínez <irving.mtqz10@yahoo.com>
2022-07-07 15:43:57 -0500

    Evento en logo Netflix

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 (function(){
   let inputEmail = document.querySelector('[name="email"]');
   let labelEmail = document.querySelector('[for="email"]');
+  const loguito = document.querySelector('#loguito');
 
   inputEmail.addEventListener('click', () => {
     labelEmail.style.marginTop = '10px';
@@ -12,6 +13,10 @@
   inputEmail.addEventListener('blur', () => {
     labelEmail.style.marginTop = '25px';
     labelEmail.style.fontSize = '15px';
+  });
+
+  loguito.addEventListener('click', () => {
+    window.location = 'index.html';
   });
 
   /* Falta alinear botón con input */
--- login/js/app.js
+++ login/js/app.js
@@ -4,10 +4,16 @@
   let moreInfo = document.querySelector('#more-info');
   let dsplyMI = document.querySelector('#dsply-mi');
 
+  const loguito = document.querySelector('#loguito');
+
   moreInfo.style.display = 'none';
 
   dsplyMI.addEventListener('click', () => {
     moreInfo.style.display = 'block';
     dsplyMI.style.display = 'none';
+  });
+
+  loguito.addEventListener('click', () => {
+    window.location = '../index.html';
   });
 })();
